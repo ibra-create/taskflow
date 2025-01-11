c1dad18b6815549bcb88baa7795f02c8a7caeb57
ibra-create <moadka123456@gmail.com>
2025-01-11 21:52:09 +0100

    Fix all dependencies and SQLAlchemy config

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from datetime import datetime
 import webview
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+with app.app_context():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(app)
+    db.create_all()
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
